36d9aa536939b89bc3b801327f416b4302416005
Anonnya-roy-majumder <79361141+Anonnya-roy-majumder@users.noreply.github.com>
2021-06-15 13:01:08 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,55 @@
+(function($) {
+    'use strict';
+
+    $('.html').rProgressbar({
+        percentage: 30,
+        fillBackgroundColor: '#bc650e',
+          backgroundColor: '#932564',
+        duration: 2000
+    });
+    $('.css').rProgressbar({
+        percentage: 50,
+        fillBackgroundColor: '#cc2e83',
+          backgroundColor: '#053d96',
+         duration: 2200
+    });
+
+
+
+    $('.php').rProgressbar({
+        percentage: 65,
+        fillBackgroundColor: '#9b59b6',
+          backgroundColor: '#11d9ea',
+         duration: 2400
+    });
+
+
+
+    $('.javascript').rProgressbar({
+        percentage: 75,
+        fillBackgroundColor: '#b73e96',
+          backgroundColor: '#5a5a55',
+         duration: 2600
+    });
+
+
+
+    $('.jquery').rProgressbar({
+        percentage: 85,
+        fillBackgroundColor: '#7c1b13',
+        backgroundColor: '#8b9325',
+         duration: 2800
+    });
+
+
+
+  $('.laravel').rProgressbar({
+        percentage: 90,
+        fillBackgroundColor: '#2a1889',
+        backgroundColor: '#837a76', 
+      duration: 3000
+    });
+
+
+
+})(jQuery);
